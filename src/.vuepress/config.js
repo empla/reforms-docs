93dd82f0271f1bbe85b5927c2926b6560f3c7dfd
Malik Zharykov <cmalikz.h@gmail.com>
2021-03-09 15:49:33 +0600

    change base

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,7 +2,7 @@ const { description } = require('../../package')
 
 module.exports = {
   dest: 'docs',
-  base: '/',
+  base: '/reforms-docs/',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
